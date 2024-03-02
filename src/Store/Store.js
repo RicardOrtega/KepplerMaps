1dfbc98069536a42ff58184216f9f3825cdd0bda
Ricardo Ortega <39242348+RicardOrtega@users.noreply.github.com>
2024-03-02 01:11:31 -0300

    Initial

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,6 +1,7 @@
 import {createStore,combineReducers,applyMiddleware} from "redux";
 import reducers from "../Reducers/Reducers"
 import {taskMiddleware} from "react-palm/tasks";
+import store from "@kepler.gl/components"
 
 
 export default store = createStore(reducers,{},applyMiddleware(taskMiddleware))
